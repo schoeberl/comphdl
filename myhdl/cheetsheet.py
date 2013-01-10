97bae26102aa60bfbc10d60517fee5930ec49b09
Martin Schoeberl <martin@jopdesign.com>
2013-01-09 17:52:36 -0800

    myhdl: combinational testing and correct signals for VHDL conversion

--- myhdl/cheetsheet.py
+++ myhdl/cheetsheet.py
@@ -1,3 +1,7 @@
+#
+# Cheatsheet fo MyHDL
+#
+
 from myhdl import *
 
 # a signed 32-bit variable/signal
@@ -11,7 +15,7 @@ print(len(unsig32), unsig32.min, unsig32.max)
 # a HW component with 2 ports
 def foo(clk, res):
 
-    val = Signal()
+    val = Signal(intbv(min=0, max=2))
 
     # a registered component
     @always(clk.posedge)
@@ -42,7 +46,61 @@ def debug(clk, res):
         print(res);
     return reg
 
-clk = Signal()
-led = Signal()
+# bool gets a std_logic (with additional conversions when used in expressions)
+clk = Signal(bool())
+# this becomes a single bit unsigned
+led = Signal(intbv(min=0, max=2))
 sim = Simulation(clk_driver(clk), foo(clk, led), debug(clk, led))
 sim.run(10)
+toVHDL(foo, clk, led)
+
+#########
+# combinational testing
+def logic(inv, out1, out2, width):
+    @always_comb
+    def orandf():
+        res = 0 # this gives an integer...
+        res2 = bool(1) # this gives an integer as well...
+        # res2 = intbv()[1:] # this does not work
+        for i in range(width):
+            res = res | inv[i]
+            res2 = res2 & inv[i]
+        out1.next = res 
+        out2.next = res2
+    return orandf
+
+def test_comb(width):
+
+    inv = Signal(intbv(0))
+    out1 = Signal(intbv(0))
+    out2 = Signal(intbv(0))
+
+    dut = logic(inv, out1, out2, width)
+
+    @instance
+    def stimulus():
+        for i in range(2**width):
+            inv.next = intbv(i)
+            yield(delay(10))
+            print("in: "+bin(inv, width)+" out1: "+bin(out1, 1)+" out2: "+bin(out2, 1))
+
+    return dut, stimulus
+
+Simulation(test_comb(width=3)).run()
+# now do the VHDL
+inv = Signal(intbv()[3:])
+out1 = Signal(bool())
+out2 = Signal(bool())
+toVHDL(logic, inv, out1, out2, 3)
+
+#######
+# bit slicing is a little bit unnatural
+val = intbv(-7, min=-8, max=7)
+x = val
+print(x, len(x), x.min, x.max)
+slice = val[3:0] # it goes from 2 to 0, not including 3
+x = slice
+print(x, len(x), x.min, x.max)
+sinslice = slice.signed()
+# this is not a intbv anymore...
+print(sinslice)
