3d6e02424da69a3d4d77bd79c380200d44e293c6
Martin Schoeberl <martin@jopdesign.com>
2013-02-08 17:06:35 -0800

    chisel: provide a minimal Hello World example (with FPGA)

--- chisel/src/example/Blink.scala
+++ chisel/src/example/Blink.scala
@@ -1,25 +1,32 @@
-// A counter
+/*
+ * This code is part of the HDL comparison.
+ * 
+ * Copyright: 2013, Technical University of Denmark, DTU Compute
+ * Author: Martin Schoeberl (martin@jopdesign.com)
+ * License: Simplified BSD License
+ * 
+ * Blinking LED: the FPGA version of Hello World
+ */
 
 package example
 
 import Chisel._
 import Node._
-import scala.collection.mutable.HashMap
 
 class Blink() extends Component {
   val io = new Bundle {
     val led = UFix(OUTPUT, 1)
   }
-  val CNT_MAX = UFix(16000000-1);
+  // BeMicro has a 16 MHz clock
+  val CNT_MAX = UFix(16000000/2-1);
   val r1 = Reg(resetVal = UFix(0, 25))
   val blk = Reg(resetVal = UFix(0, 1))
+  r1 := r1 + UFix(1)  
   when (r1 === CNT_MAX) {
     r1 := UFix(0)
     blk := ~blk
   }
-  r1 := r1 + UFix(1)  
   io.led := blk
-
 }
 
 // The sbt build tool passes some arguments to our main,
--- hello-chisel/src/Hello.scala
+++ hello-chisel/src/Hello.scala
@@ -1,30 +1,39 @@
-// Hello World Chisel/Scala
+/*
+ * This code is a minimal hardware described in Chisel.
+ * 
+ * Copyright: 2013, Technical University of Denmark, DTU Compute
+ * Author: Martin Schoeberl (martin@jopdesign.com)
+ * License: Simplified BSD License
+ * 
+ * Blinking LED: the FPGA version of Hello World
+ */
 
-// shouldn't the (Scala) source tree be organized in packets as in Java?
-
-package example
+package hello
 
 import Chisel._
 import Node._
 
-
-class Adder(size: Int) extends Component
-{
+// The blinking LED componenet
+// BeMicro has a 16 MHz clock
+class Hello() extends Component {
   val io = new Bundle {
-    val op1 = UFix(INPUT, size)
-    val op2 = UFix(INPUT, size)
-    val out = UFix(OUTPUT, size)
+    val led = UFix(OUTPUT, 1)
   }
-
-  io.out := io.op1 + io.op2
-
+  val CNT_MAX = UFix(16000000/2-1);
+  val r1 = Reg(resetVal = UFix(0, 25))
+  val blk = Reg(resetVal = UFix(0, 1))
+  
+  r1 := r1 + UFix(1)  
+  when (r1 === CNT_MAX) {
+    r1 := UFix(0)
+    blk := ~blk
+  }
+  io.led := blk
 }
-
-// The sbt build tool passes some arguments to our main,
-// which gives some arguments for the Chisel code generation
-
+  
+// Generate the Verlog code by invoking chiselMain() in our main()
 object HelloMain {
   def main(args: Array[String]): Unit = { 
-    chiselMain( args, () => new Adder(10));
+    chiselMain( args, () => new Hello())
   }
 }
