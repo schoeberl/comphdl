30e42c7e91436c640c9c7cbd6464592be0bf4c36
Martin Schoeberl <martin@jopdesign.com>
2013-02-11 21:30:17 -0800

    chisel: issue with combinational expression and when

---
+++ chisel/src/error/CombError.scala
@@ -0,0 +1,86 @@
+/*
+ * This code is part of the HDL comparison.
+ * 
+ * Copyright: 2013, Technical University of Denmark, DTU Compute
+ * Author: Martin Schoeberl (martin@jopdesign.com)
+ * License: Simplified BSD License
+ * 
+ * Shows a problem with Chisel.
+ */
+
+package error
+
+import Chisel._
+import Node._
+
+import scala.collection.mutable.HashMap
+
+class ErrorContainer() extends Component {
+  val io = new Bundle {
+    val led = Bits(OUTPUT, 8)
+  }
+  
+  var tck = new CombErr();
+  
+  val led = Reg(resetVal = Bits(0, 8))
+  
+  when (tck.io.ticka === Bits(1) || tck.io.tickb === Bits(1)) {
+    led := ~led
+  }
+  io.led := led
+}
+
+/**
+ * Generate a 2 Hz tick to drive the FSM input test bench.
+ */
+class CombErr() extends Component {
+  val io = new Bundle {
+    val ticka = Bits(OUTPUT, 1)
+    val tickb = Bits(OUTPUT, 1)
+  }
+
+  val CNT_MAX = UFix(3);
+  
+  val r1 = Reg(resetVal = UFix(0, 25))
+  
+  val limit = r1 === CNT_MAX
+
+  // The following is the error: both variables have the same value:
+  // 0 when limit is true, 1 when false
+  val ticka = when(limit) { Bits(0) } .otherwise { Bits(1) }
+  val tickb = when(limit) { Bits(1) } .otherwise { Bits(0) }
+  
+      r1 := r1 + UFix(1)
+  when (limit) {
+    r1 := UFix(0)
+  }
+  
+  io.ticka := ticka
+  io.tickb := tickb
+}
+
+class ErrorTest(ec: ErrorContainer) extends Tester(ec, Array(ec.io)) {
+  defTests {
+    val ret = true
+    val vars = new HashMap[Node, Node]()
+    val ovars = new HashMap[Node, Node]()
+
+    for (i <- 0 until 10) {
+      step(vars, ovars)
+      println("iter: "+i)
+      println("ovars: "+ovars)
+      println(" led/litVal "+ovars(c.io.led).litValue())
+    }
+
+    ret
+  }
+}
+
+object CombErrMain {
+  def main(args: Array[String]): Unit = {
+    chiselMainTest(args, () => new ErrorContainer()) {
+      f => new ErrorTest(f)
+    }
+
+  }
+}
--- chisel/src/example/Fsm.scala
+++ chisel/src/example/Fsm.scala
@@ -18,17 +18,19 @@ import scala.collection.mutable.HashMap
 class FsmContainer() extends Component {
   val io = new Bundle {
     val led = Bits(OUTPUT, 8)
+    val cnt = UFix(OUTPUT, 25)    
   }
   
   var tck = new Tick();
   
   val led = Reg(resetVal = Bits(0, 8))
   
-  when (tck.tick === Bits(1)) {
+  when (tck.io.ticka === Bits(1) || tck.io.tickb === Bits(1)) {
     // led := Cat(led(6, 0), led(7))
     led := ~led
   }
   io.led := led
+  io.cnt := tck.io.cnter
 }
 
 /**
@@ -36,33 +38,57 @@ class FsmContainer() extends Component {
  */
 class Tick() extends Component {
   val io = new Bundle {
-    val tick = Bits(OUTPUT, 1)
+    val ticka = Bits(OUTPUT, 1)
+    val tickb = Bits(OUTPUT, 1)
+    val cnter = UFix(OUTPUT, 25)
   }
   // BeMicro has a 16 MHz clock
-  val CNT_MAX = UFix(3) // UFix(16000000/2-1);
+  val CNT_MAX = UFix(16000000/2-1);
   
   val r1 = Reg(resetVal = UFix(0, 25))
   
   val limit = r1 === CNT_MAX
-  val tick = Mux(limit, Bits(0), Bits(1))
+//  val ticka = limit
+  
+//  val ticka = when(limit) { Bits(0) } .otherwise { Bits(1) }
+//  val tickb = when(limit) { Bits(1) } .otherwise { Bits(0) }
+  
+  val ticka = Bits(width=1)
+  ticka := Bits(0)
+  when (limit) { ticka := Bits(0, 1) } .otherwise{ ticka := Bits(1, 1) }
+  val tickb = Bits(width=1)
+  tickb := Bits(0)
+  when (limit) { tickb := Bits(1, 1) } .otherwise{ tickb := Bits(0, 1) }
+    
+  // Do I like MUX? Probably not
+  // val ticka = Mux(limit, Bits(1), Bits(0))
+  // no if in Chisel
+  // val ticka = if (limit) then Bits(1) else Bits(0)
 
   r1 := r1 + UFix(1)
   when (limit) {
     r1 := UFix(0)
   }
   
+  io.ticka := ticka
+  io.tickb := tickb
   
-  io.tick := tick
+  io.cnter := r1
 }
 
 class FsmTest(fsm: FsmContainer) extends Tester(fsm, Array(fsm.io)) {
   defTests {
     val ret = true
     val vars = new HashMap[Node, Node]()
+    val ovars = new HashMap[Node, Node]()
 
-    for (i <- 0 until 5) {
-      step(vars, isTrace = false)
-      println(fsm.io.led)
+    for (i <- 0 until 10) {
+      vars.clear // ?? why?
+      step(vars, ovars)
+      println("iter: "+i)
+      println("ovars: "+ovars)
+      println("cnt/litValue: "+ovars(c.io.cnt).litValue()+" led/litVal "+ovars(c.io.led).litValue())
+//      println(vars(fsm.io.led))
     }
 
     ret
